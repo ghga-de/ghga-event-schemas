cb4c9e4a6e4a4ec8568466fb24387e495cf0abe1
Byron Himes <byro93@live.com>
2023-04-13 11:48:04 +0200

    Add Notification event (GSI 42) (#31)
    * Update template files
    
    * Update dependencies
    
    * Update license blocks
    
    * Add EmailNotification event
    
    * Bump init version
    
    * Rename EmailNotification to Notification
    
    ---------
    
    Co-authored-by: Byron Himes <TheByronHimes@gmail.com>
    Co-authored-by: KerstenBreuer <kersten-breuer@outlook.com>

--- ghga_event_schemas/pydantic_.py
+++ ghga_event_schemas/pydantic_.py
@@ -21,7 +21,7 @@ schema representations such as json-schema.
 
 from typing import Optional
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, EmailStr, Field, validator
 
 from ghga_event_schemas.validation import validated_upload_date
 
@@ -301,6 +301,34 @@ class FileDownloadServed(NonStagedFileRequested):
         title = "file_download_served"
 
 
+class Notification(BaseModel):
+    """This event is emitted by services that desire to send a notification.
+    It is picked up by the notification service."""
+
+    recipient_email: EmailStr = Field(
+        ..., description="The primary recipient of the email"
+    )
+    email_cc: list[EmailStr] = Field(
+        default=[], description="The list of recipients cc'd on the email"
+    )
+    email_bcc: list[EmailStr] = Field(
+        default=[], description="The list of recipients bcc'd on the email"
+    )
+    subject: str = Field(..., description="The subject line for the notification")
+    recipient_name: str = Field(
+        ...,
+        description="The full name of the recipient to be used in the greeting section",
+    )
+    plaintext_body: str = Field(
+        ..., description="The basic text for the notification body"
+    )
+
+    class Config:
+        """Additional Model Config."""
+
+        title = "notification"
+
+
 # Lists event schemas (values) by event types (key):
 schema_registry: dict[str, type[BaseModel]] = {
     "metadata_dataset_overview": MetadataDatasetOverview,
@@ -313,4 +341,5 @@ schema_registry: dict[str, type[BaseModel]] = {
     "non_staged_file_requested": NonStagedFileRequested,
     "file_staged_for_download": FileStagedForDownload,
     "file_download_served": FileDownloadServed,
+    "notification": Notification,
 }
