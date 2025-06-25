6091fb10d2254173548953ef0a1d35c135f64c3f
TheByronHimes <TheByronHimes@gmail.com>
2025-06-25 19:28:32 +0000

    Add support for whole artifact events and bump to 9.2.0

--- src/ghga_event_schemas/pydantic_.py
+++ src/ghga_event_schemas/pydantic_.py
@@ -73,6 +73,25 @@ class SearchableResource(SearchableResourceInfo):
     )
 
 
+class ArtifactTag(BaseModel):
+    """A model representing a tag for an artifact (artifact name and submission ID)."""
+
+    submission_id: str = Field(
+        ..., description="The ID of the submission this artifact belongs to."
+    )
+    artifact_name: str = Field(
+        ..., description="The name of the artifact, e.g. 'added_accessions'."
+    )
+
+
+class Artifact(ArtifactTag):
+    """A model representing an artifact."""
+
+    content: dict[str, Any] = Field(
+        ..., description="The metadata content of the artifact."
+    )
+
+
 class MetadataDatasetOverview(MetadataDatasetID):
     """
     Overview of files contained in a dataset.
